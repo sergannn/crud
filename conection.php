47d1eef336fdf38afb65d48696489d3a47a7d5fc
sergannn <su.vasiliev@gmail.com>
2024-02-02 20:36:45 +0300

    Add files via upload
    ss

---
+++ conection.php
@@ -0,0 +1,54 @@
+<?php
+
+ if (class_exists('PDO')) {
+   // echo 'PDO установлен на этом сервере.';
+} else {
+  //  echo 'PDO не установлен на этом сервере.';
+}
+    
+    class crud{
+        public static function conect()
+        {
+           try {
+         $con=new PDO('mysql:localhost=h315225216.mysql; dbname=h315225216_db','h315225216_ser','Leendows87');
+            return $con;
+           } catch (PDOException $error1) {
+                echo 'Something went wrong, with you conection!'.$error1->getMessage();
+           }catch (Exception $error2){
+                 echo 'Generic error!'.$error2->getMessage();
+           }
+        }
+        public static function Selectdata()
+        {
+            $data=array();
+            $p=crud::conect()->prepare('SELECT * FROM crudtable');
+            $p->execute();
+           $data=$p->fetchAll(PDO::FETCH_ASSOC);
+           return $data;
+        }
+        public static function delete($id)
+        {
+            $p=crud::conect()->prepare('DELETE FROM crudtable WHERE id=:id');
+            $p->bindValue(':id',$id);
+            $p->execute();
+        }
+public static function userDataPerId($id)
+{
+    $data=array();
+    $p=crud::conect()->prepare('SELECT * FROM crudtable WHERE id=:id');
+    $p->bindValue(':id',$id);
+    $p->execute();
+   $data=$p->fetch(PDO::FETCH_ASSOC);
+   return $data;
+}
+
+
+
+
+    }
+
+
+
+
+
+?>
